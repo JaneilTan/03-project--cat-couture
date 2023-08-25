f49c7f6655bb452ef86089cf73dbc9f9cba4e985
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-08-25 15:32:39 +1200

    Passed limit and page as parameters for getProducts function

--- server/products/product.repository.js
+++ server/products/product.repository.js
@@ -1,10 +1,11 @@
-const db = require("../db"); 
+const db = require("../db");
 
 module.exports = {
-    getProducts: async () => {
-        try {
-          const result = await db.query(
-            `SELECT
+  getProducts: async (limit, page) => {
+    try {
+      const offset = limit * (page - 1);
+      const result = await db.query(
+        `SELECT
               p.id,
               p.name,
               p.description,
@@ -17,17 +18,19 @@ module.exports = {
             LEFT JOIN product_image pi ON p.product_image_id = pi.id
             ORDER BY
               p.id
-            `
-          );
-          return result.rows;
-        } catch (error) {
-          throw Error(error);
-        }
-      },
-    getTotalProducts: async () => {
-        try {
-            const result = await db.query(
-                `WITH ProductCTE AS (
+            LIMIT $1 OFFSET $2
+            `,
+            [limit, offset]
+      );
+      return result.rows;
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+  getTotalProducts: async () => {
+    try {
+      const result = await db.query(
+        `WITH ProductCTE AS (
                     SELECT
                         p.id,
                         p.name,
@@ -39,17 +42,16 @@ module.exports = {
                     FROM product p
                     LEFT JOIN product_category pc ON p.product_category_id = pc.id
                     LEFT JOIN product_image pi ON p.product_image_id = pi.id
-                    -- Add any additional filters or conditions here if needed
                 )
                 SELECT
                     *,
                     (SELECT COUNT(*) FROM ProductCTE) AS totalProducts
                 FROM ProductCTE
                 ORDER BY ProductCTE.id;`
-            );
-            return result.rows;
-          } catch (error) {
-            throw Error(error);
-        }
-      },
+      );
+      return result.rows;
+    } catch (error) {
+      throw Error(error);
+    }
+  },
 };
--- server/products/product.router.js
+++ server/products/product.router.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const Joi = require("joi");
 const router = express.Router();
-const db = require("../db");
 const queryParamValidationMiddleware = require("../middleware/queryParamValidationMiddleware");
 const productRepository = require("./product.repository");
 
@@ -9,30 +8,6 @@ const queryParamsSchema = Joi.object().keys({
   page: Joi.number().integer().min(1),
   limit: Joi.number().integer().min(1),
 });
-
-const getProducts = async () => {
-  try {
-    const result = await db.query(
-      `SELECT
-        p.id,
-        p.name,
-        p.description,
-        p.price,
-        pc.name AS "categoryName",
-        pi.name AS "imageName",
-        pi.description AS "imageDescription"
-      FROM product p
-      LEFT JOIN product_category pc ON p.product_category_id = pc.id
-      LEFT JOIN product_image pi ON p.product_image_id = pi.id
-      ORDER BY
-        p.id
-      `
-    );
-    return result.rows;
-  } catch (error) {
-    throw Error(error);
-  }
-};
 
 router.get(
   "/",
